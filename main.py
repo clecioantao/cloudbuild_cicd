6ac6685bc25ac0be0391295281b27c374b04ec71
Clecio Antao <clecio.antao@gmail.com>
2024-06-04 06:35:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ client = bigquery.Client()
 
 @app.route('/')
 def main(big_query_client=client):
+    
     table_id = "labcle011.test_schema.us_states"
     job_config = bigquery.LoadJobConfig(
         write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
